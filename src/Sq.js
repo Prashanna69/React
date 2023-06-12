c9981a1702902c5bbb70ecb8a43d4b095409037d
Prashanna Lohani <np03cs4s220275@heraldcollege.edu.np>
2023-06-12 12:29:24 +0545

    Color defining System

---
+++ src/Sq.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sq = (colorValue) => {
+  return (
+    <section className="square" style={{ backgroundColor: colorValue }}>
+      <p>{colorValue ? colorValue : "Empty value"}</p>
+    </section>
+  );
+};
+Sq.defaultProps = {
+  colorValue: "Empty value",
+};
+
+export default Sq;
